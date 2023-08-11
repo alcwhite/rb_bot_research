7e9d2bd7f2179818e2fdf411bd2afa5879a7857e
Al White <al@launchscout.com>
2023-08-11 14:29:07 -0400

    Get a text modal working

--- lib/rb_bot_research/components.rb
+++ lib/rb_bot_research/components.rb
@@ -9,26 +9,35 @@ module RbBotResearch
         bot.message(contains: /(ct)/) { |event| send_messages(bot, event) }
 
         bot.string_select do |event|
-          event.channel.send_message(
-            "**[STRING_SELECT]**\nYou have chosen the values: **#{event.values.join('**, **')} for selection #{event.custom_id}**", false, nil, nil, nil, nil, nil
-          )
-          event.interaction.respond(content: "Value selected", ephemeral: true)
+          event.interaction.respond(content: "You have chosen the values: **#{event.values.join('**, **')}** for selection #{event.custom_id}")
+        end
+
+        bot.button do |event|
+          event.show_modal(title: 'Test modal', custom_id: 'test1234') do |modal|
+            modal.row do |r|
+                r.text_input(style: 1, custom_id: "description", label: "Write your description", min_length: 10, max_length: 1000, required: true)
+            end
+          end
+          event.interaction.respond(content: "#{event.custom_id} Button pushed")
+        end
+
+        bot.modal_submit custom_id: 'test1234' do |event|
+          print(event.inspect)
+          print(event.value('description'))
+          event.respond(content: "Thanks for submitting your modal. You sent characters.")
         end
       end
 
       def send_messages(bot, event)
-        # event.channel.send_message("HI THERE text", false, nil, nil, nil, nil,
-        #   Discordrb::Webhooks::Modal.new do |builder|
-        #     builder.row do |r|
-        #       r.text_input(style: 2, custom_id: "description", label: "Write your description", min_length: 10, max_length: 1000, required: true)
-        #     end
+        # event.show_modal(title: 'Test modal', custom_id: 'test1234') do |modal|
+        #   modal.row do |r|
+        #       r.text_input(style: 1, custom_id: "description", label: "Write your description", min_length: 10, max_length: 1000, required: true)
         #   end
-        # )
+        # end
 
         event.channel.send_message("HI THERE", false, nil, nil, nil, nil,
           Discordrb::Webhooks::View.new do |builder|
             builder.row do |r|
-              # r.text_input(style: 2, custom_id: "description", label: "Write your description", min_length: 10, max_length: 1000, required: true)
               r.string_select(custom_id: 'string_select', placeholder: 'Test of StringSelect', max_values: 1, min_values: 1) do |ss|
                         ss.option(label: 'Value 1', value: '1', description: 'First value', emoji: { name: '1️⃣' })
                         ss.option(label: 'Value 2', value: '2', description: 'Second value', emoji: { name: '2️⃣' })
@@ -42,11 +51,6 @@ module RbBotResearch
                         ss.option(label: 'Value 10', value: '10', description: 'Tenth value', emoji: { name: '🔟' })
                       end
             end
-          end
-        )
-
-        event.channel.send_message("HI THERE #2", false, nil, nil, nil, nil,
-          Discordrb::Webhooks::View.new do |builder|
             builder.row do |r|
               r.string_select(custom_id: 'string_select_2', placeholder: 'Test of StringSelect2', max_values: 1, min_values: 1) do |ss|
                         ss.option(label: 'Value 1', value: '1', description: 'First value', emoji: { name: '1️⃣' })
@@ -60,6 +64,9 @@ module RbBotResearch
                         ss.option(label: 'Value 9', value: '9', description: 'Ninth value', emoji: { name: '9️⃣' })
                         ss.option(label: 'Value 10', value: '10', description: 'Tenth value', emoji: { name: '🔟' })
                       end
+            end
+            builder.row do |r|
+              r.button(custom_id: "submit", style: 1, label: "Add details and submit Data")
             end
           end
         )
